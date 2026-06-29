b4ef6b1ee3caede615db77262f0f6966a46bdc13
Andrea <80144630+ndrxxz@users.noreply.github.com>
2026-06-29 15:09:09 +0800

    add formatDate function

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import { EmptyState } from "@/components/ui";
 import { RiDeleteBinLine } from "react-icons/ri";
 
-function ExpenseList() {
+function ExpenseList({ expenses }) {
+  const formatDate = (date) => {
+    return new Date(date).toLocaleDateString("en-US", {
+      month: "short",
+      day: "2-digit",
+    });
+  };
+
+  console.log(expenses);
+
   return (
     <div className="bg-white/5 border border-white/10 rounded-xl p-5 flex flex-col gap-3">
       <div className="text-xs font-semibold text-zinc-500 uppercase tracking-wide">
